7e56b1c8137cfb0668fa50bef501fd362ef0a0b7
farout101 <phyozawlinn1852020@gmail.com>
2025-10-13 16:56:32 +0630

    scraping first try

--- burmese_news/excel_converter.py
+++
@@ -1,3 +0,0 @@
-import pandas as pd
-df = pd.read_csv('data.csv')
-df.to_excel('burmese_news.xlsx', index=False)
--- burmese_news/pipelines.py
+++ burmese_news/pipelines.py
@@ -8,6 +8,6 @@
 from itemadapter import ItemAdapter
 
 
-class BurmesNewsPipeline:
+class BurmeseNewsPipeline:
     def process_item(self, item, spider):
         return item
